a0e75c715a327942cebdd88be93035542080652a
TimurCherin <109847059+TimurCherin@users.noreply.github.com>
2023-08-01 19:33:16 +0300

    hw-js-19

---
+++ js/index.js
@@ -0,0 +1,19 @@
+                                                        // ex 1
+const array2 = ['apple', 'banana', 'orange', 'banana', 'grape', 'apple', 'kiwi'];
+function changeArray (array) {
+    const newArray = array.reduce((acc, arr) => { 
+        acc.push(arr[0] + arr[arr.length - 1])
+    return acc
+},[])
+    return newArray
+}
+console.log(changeArray(array2))
+                                                        // ex 2
+const people = [
+{ name: 'John', surname: "Doe", age: 32, occupation: 'programmer' },
+{ name: 'Jane', surname: "Lee", age: 26, occupation: 'teacher' },
+{ name: 'Mike', surname: "Watson", age: 42, occupation: 'engineer' },
+{ name: 'Emily', surname: "Brad", age: 29, occupation: 'designer' }
+];
+const myArray = people.map((obj) => {return {fullName: `${obj.name} ${obj.surname}`, id: obj.age}})
+console.log(myArray)
